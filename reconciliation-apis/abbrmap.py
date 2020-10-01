5e0433bcc9c05e42402c8a757b840d6a25166087
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-10-01 12:44:24 -0600

    add simple reconciliation API

---
+++ reconciliation-apis/abbrmap.py
@@ -0,0 +1,35 @@
+abbrmap = {
+    "ahu": ["ahu"],
+    "avg": ["average"],
+    "cmd": ["command"],
+    "elec": ["electrical"],
+    "equip": ["equipment"],
+    "freq": ["frequency"],
+    "occ": ["occupied"],
+    "rtu": ["rtu"],
+    "roof": ["rooftop"],
+
+    "sp": ["setpoint"],
+    "spt": ["setpoint"],
+    "stpt": ["setpoint"],
+
+    "temp": ["temperature"],
+    "unocc": ["unoccupied"],
+    "volt": ["voltage"],
+    "ctl": ["control"],
+
+    "cfm": ["flow"],
+
+    "sa": ["supply", "air"],
+    "ea": ["exhaust", "air"],
+    "da": ["discharge", "air"],
+
+    "sup": ["supply"],
+    "dis": ["discharge"],
+
+    "hw": ["hot", "water"],
+    "chw": ["chilled", "water"],
+
+    "z": ["zone"],
+    "zn": ["zone"],
+}

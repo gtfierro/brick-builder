d61d8ec9686300a431a335b38187714ecf2142f1
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-11-03 09:45:33 -0800

    add more mappings

--- reconciliation-apis/abbrmap.py
+++ reconciliation-apis/abbrmap.py
@@ -23,9 +23,32 @@ abbrmap = {
     "sa": ["supply", "air"],
     "ea": ["exhaust", "air"],
     "da": ["discharge", "air"],
+    "oa": ["outside", "air"],
+    "ra": ["return", "air"],
 
+    "sat": ["supply", "air", "temperature"],
+    "eat": ["exhaust", "air", "temperature"],
+    "dat": ["discharge", "air", "temperature"],
+    "oat": ["outside", "air", "temperature"],
+    "rat": ["return", "air", "temperature"],
+
+    "sap": ["supply", "air", "pressure"],
+    "eap": ["exhaust", "air", "pressure"],
+    "dap": ["discharge", "air", "pressure"],
+    "oap": ["outside", "air", "pressure"],
+    "rap": ["return", "air", "pressure"],
+
+    "sf": ["supply", "fan"],
+    "ef": ["exhaust", "fan"],
+    "df": ["discharge", "fan"],
+    "of": ["outside", "fan"],
+    "rf": ["return", "fan"],
+
+    "t": ["temperature"],
     "sup": ["supply"],
     "dis": ["discharge"],
+    "ex": ["exhaust"],
+    "ret": ["return"],
 
     "hw": ["hot", "water"],
     "chw": ["chilled", "water"],
